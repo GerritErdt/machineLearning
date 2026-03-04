6ee7e61490be36bd6e5445ee82edc819050c67d1
Gerrit Erdt <gerrit.erdt@gmail.com>
2026-03-04 10:24:00 +0100

    parameters for fabian

--- src/main.py
+++ src/main.py
@@ -4,19 +4,10 @@ import src.helperFunctions.helper_functions as hf
 
 def main():
     hf.set_all_seeds()
-    train_loader, test_loader, pos_weight_val = dl.get_stereo_clean_dataset(50000, batch_size=128)
+    train_loader, test_loader, pos_weight_val = dl.get_stereo_clean_dataset(75000, batch_size=128)
     
     ml_model = model.GNNModel()
-    trained_model, history = model.learn(ml_model, train_loader, test_loader, 10, pos_weight=pos_weight_val)
-
-    batch_data = next(iter(test_loader))
-    ml_model.save(
-        batch_data.x,
-        batch_data.edge_index,
-        batch_data.batch,
-        batch_data.num_graphs,
-        path="./model.onnx"
-    )
+    trained_model, history = model.learn(ml_model, train_loader, test_loader, 25, pos_weight=pos_weight_val)
         
     hf.plot_history(history)
         
